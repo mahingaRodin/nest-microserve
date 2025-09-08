c75c4cbe50ebfe19d63c92567bd28a8ee116ebe9
P-Odiix <mahingarodin@gmail.com>
2025-09-08 13:29:06 +0200

    [re-instantiated and solved the mongodb conn.]

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import * as Joi from 'joi';
-import { ConfigModule } from "@nestjs/config";
-import { DatabaseModule } from "@app/common";
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './order.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -14,9 +17,10 @@ import { DatabaseModule } from "@app/common";
       }),
       envFilePath: './apps/orders/.env',
     }),
-    DatabaseModule
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
