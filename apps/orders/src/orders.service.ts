476d00d17bcbb85dd3bb3a79e5f67dff06a6b64e
P-Odiix <mahingarodin@gmail.com>
2025-09-17 19:50:42 +0200

    [billing service ✅ ]

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { OrdersRepository } from './order.repository';
 import { CreateOrderRequest } from './dto/create-order.request';
+import { BILLING_SERVICE } from './constants/service';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from "rxjs";
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly orderRepository: OrdersRepository) {}
+  constructor(
+    private readonly orderRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
-    return this.orderRepository.create(request);
+    const sesssion = await this.orderRepository.startTransaction();
+    try {
+
+      const order = await this.orderRepository.create(request, { session: sesssion })
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request
+        })
+      );
+      await sesssion.commitTransaction();
+      return order;
+    } catch (err) {
+      await sesssion.abortTransaction();
+      throw err;
+    }
   }
 
   async getOrders() {
