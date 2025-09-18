994844228195498661a0d3e0313518b6e43dd86e
P-Odiix <mahingarodin@gmail.com>
2025-09-18 09:59:24 +0200

    [auth-service ✅ ]

--- libs/common/src/rmq/rmq.service.ts
+++ libs/common/src/rmq/rmq.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { RmqOptions, Transport } from '@nestjs/microservices';
+import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class RmqService {
@@ -10,11 +10,19 @@ export class RmqService {
     return {
       transport: Transport.RMQ,
       options: {
-        urls: [this.configService.get<string>('RABBIT_MQ_URI') ?? 'amqp://localhost'],
+        urls: [
+          this.configService.get<string>('RABBIT_MQ_URI') ?? 'amqp://localhost',
+        ],
         queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
         noAck,
         persistent: true,
       },
     };
+  }
+
+  ack(context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
   }
 }
